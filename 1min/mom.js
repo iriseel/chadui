8082375e17e2799a2b4abd428a82caa775d57a5b
iriseel <74837161+iriseel@users.noreply.github.com>
2022-10-04 11:36:05 -0400

    诞生！

---
+++ 1min/mom.js
@@ -0,0 +1,70 @@
+var questions = document.querySelectorAll(".question");
+//var clicks = -1;
+var audio = document.querySelector("audio");
+
+    
+window.addEventListener('click', function(){
+
+    questions[0].classList.add('show');
+
+    audio.play();
+    
+    var myTimeout = setTimeout(question_1, 5871);
+});
+
+
+function clear(){
+    for(var i = 0; i < questions.length; i++){
+        questions[i].classList.add('hidden');
+        questions[i].classList.remove('show');
+    }
+};
+
+
+function question_1() { 
+    clear();
+    questions[1].classList.add('show');
+    var myTimeout = setTimeout(question_2, 3747);
+
+};
+
+function question_2() { 
+    clear();
+    questions[2].classList.add('show');
+    var myTimeout = setTimeout(question_3, 4460);
+
+};
+
+function question_3() { 
+    clear();
+    questions[3].classList.add('show');
+    var myTimeout = setTimeout(question_4, 3990);
+
+};
+
+function question_4() { 
+    clear();
+    questions[4].classList.add('show');
+    var myTimeout = setTimeout(question_5, 4833);
+
+};
+
+function question_5() { 
+    clear();
+    questions[5].classList.add('show');
+    var myTimeout = setTimeout(question_6, 11951);
+
+};
+
+function question_6() { 
+    clear();
+    questions[6].classList.add('show');
+    var myTimeout = setTimeout(question_end, 11000);
+
+};
+
+function question_end() { 
+    clear();
+
+};
+
---
+++ 1min/script.js
@@ -0,0 +1,90 @@
+var narrations = document.querySelectorAll(".narration");
+//var clicks = -1;
+var audio = document.querySelector("audio");
+var video = document.querySelector("video");
+
+
+document.querySelector('video').playbackRate = 0.6;
+    
+window.addEventListener('click', function(){
+
+    narrations[0].classList.add('show');
+
+    audio.play();
+    video.play();
+    
+    var myTimeout = setTimeout(narration_1, 5871);
+});
+
+
+function clear(){
+    for(var i = 0; i < narrations.length; i++){
+        narrations[i].classList.add('hidden');
+        narrations[i].classList.remove('show');
+    }
+};
+
+
+function narration_1() { 
+    clear();
+    narrations[1].classList.add('show');
+    var myTimeout = setTimeout(narration_2, 3747);
+
+};
+
+function narration_2() { 
+    clear();
+    narrations[2].classList.add('show');
+    var myTimeout = setTimeout(narration_3, 4460);
+
+};
+
+function narration_3() { 
+    clear();
+    narrations[3].classList.add('show');
+    var myTimeout = setTimeout(narration_4, 3990);
+
+};
+
+function narration_4() { 
+    clear();
+    narrations[4].classList.add('show');
+    var myTimeout = setTimeout(narration_5, 4833);
+
+};
+
+function narration_5() { 
+    clear();
+    narrations[5].classList.add('show');
+    var myTimeout = setTimeout(narration_6, 11951);
+
+};
+
+function narration_6() { 
+    clear();
+    narrations[6].classList.add('show');
+    var myTimeout = setTimeout(narration_end, 11000);
+
+};
+
+function narration_end() { 
+    clear();
+    video.pause();
+
+};
+
+
+
+//window.addEventListener('click', function(){
+//    clicks++;
+//
+//    for(var i = 0; i < narrations.length; i++){
+//        narrations[i].classList.add('hidden');
+//        narrations[i].classList.remove('show');
+//    }
+//
+//    narrations[clicks].classList.add('show');
+//
+//    audio.play();
+//
+//});
